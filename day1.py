b88b022da66aa4560266fdcb0a94c44ff889ac3e
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-03-04 02:58:46 +0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,10 @@
+username = input("Enter your username : ")
+passWord = input("Enter your Password : ")
+rull = input("Enter your class rull : ")
+
+
+print("-------------------------OUTPUT--------------------------")
+print("Your Name is : ",username)
+print("Your password : ",passWord)
+print("Your class rull : ",rull)
 
