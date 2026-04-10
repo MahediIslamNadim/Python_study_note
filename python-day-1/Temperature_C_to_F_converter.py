ab72b84dbff1f74d9a0c5f37f66f73e424629b3e
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-04-11 00:23:00 +0600

    Delete python-day-1/Temperature_C_to_F_converter.py

--- python-day-1/Temperature_C_to_F_converter.py
+++
@@ -1,9 +0,0 @@
-#  F=(C*(9/5))+32
-# input celsius 
-celsius = float(input("Enter you Temperature °C :"))
-
-# Fahrenheit formola
-fahrenheit = (celsius * (9/5) + 32 )
-
-#out put fahrenheit 
-print(f"{celsius}°C = {fahrenheit}°F")
