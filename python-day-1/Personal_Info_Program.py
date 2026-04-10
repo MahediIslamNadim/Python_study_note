9ef2015836b6d7b7d35237a431b8db1750685da4
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-04-11 00:23:50 +0600

    Delete python-day-1/Personal_Info_Program.py

--- python-day-1/Personal_Info_Program.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter Your Name : ")
-age = input("Enter Your Age : ")
-address = input("Enter Your address : ")
-
-print("---------------------personal info program------------------------")
-print("your Name is ",name)
-print("your age ", age)
-print("your address ",address)
